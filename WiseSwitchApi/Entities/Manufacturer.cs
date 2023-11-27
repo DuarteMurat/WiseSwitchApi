24c4693c431d0db9caf506b6254f213e2907bb7b
DuarteMurat <duarte.murat@gmail.com>
2023-11-27 14:03:28 +0000

    Add project files.

---
+++ WiseSwitchApi/Entities/Manufacturer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace WiseSwitch.Data.Entities
+{
+    [Index(nameof(Name), IsUnique = true)]
+    public class Manufacturer : IEntity
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+
+        public IEnumerable<Brand> Brands { get; set; }
+    }
+}
---
+++ WiseSwitchApi/Entities/ProductSeries.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace WiseSwitch.Data.Entities
+{
+    [Index(nameof(Name), IsUnique = true)]
+    public class ProductSeries : IEntity
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+
+        public int ProductLineId { get; set; }
+
+        public ProductLine ProductLine { get; set; }
+    }
+}
